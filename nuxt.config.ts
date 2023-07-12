d028c2601ada00f10d252fabf3e9e0d07b9a1f89
Widdy LOUIS <louisws@outlook.fr>
2023-07-12 23:44:48 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@unocss/nuxt'],
+  unocss: {
+    uno: true,
+    preflight: true
+  },
   devtools: { enabled: true }
 })
