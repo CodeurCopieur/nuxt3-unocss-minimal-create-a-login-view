3ca19c715704d03fda6497c49fed02ca0c3cf474
Widdy LOUIS <louisws@outlook.fr>
2023-07-12 23:44:28 +0200

    Create BackgroundColor.vue

---
+++ components/BackgroundColor.vue
@@ -0,0 +1,14 @@
+<script setup>
+  const route = useRoute();
+  const img = ref('01.jpg');
+</script>
+
+<template>
+ <img loading="lazy" :src="img" class="h-full object-cover w-full absolute top-0 left-0 -z-1 animate-fade-in animate-duration-3000">
+</template>
+
+
+
+<style lang="scss" scoped>
+  
+</style>
