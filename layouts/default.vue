2cbe35fb1b907f02b37b60af594f5cb04c1d00d2
Widdy LOUIS <louisws@outlook.fr>
2023-07-12 23:44:40 +0200

    Create default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- <div class="flex flex-col bg-gray-900 text-white"> -->
+ 
+     <!-- <Header/> -->
+     <main class="w-full min-h-screen relative">
+        <div class="relative">
+          <slot />
+        </div>
+     </main>
+     <!-- <Footer/> -->
+  <!-- </div> -->
+ </template>
