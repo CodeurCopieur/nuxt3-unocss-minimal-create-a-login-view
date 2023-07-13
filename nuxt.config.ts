76a8afd36c0758c05c5a3bcaaa734032024a0eaf
Widdy LOUIS <louisws@outlook.fr>
2023-07-13 22:03:36 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,11 @@ export default defineNuxtConfig({
   modules: ['@unocss/nuxt'],
   unocss: {
     uno: true,
-    preflight: true
+    preflight: true,
+    shortcuts: {
+      button: 'rounded-md font-sans flex font-medium bg-gray-900 text-lg w-full py-2 text-gray-50 underline-transparent items-center justify-center',
+      'button-secondary': 'rounded-md font-sans flex font-medium text-lg w-full text-gray-900 underline-transparent items-center justify-center'
+    }
   },
   devtools: { enabled: true }
 })
