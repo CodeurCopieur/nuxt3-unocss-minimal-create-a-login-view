41453a7c8c42937058ccfd5a2c41d4e810954440
Widdy LOUIS <louisws@outlook.fr>
2023-07-13 22:03:02 +0200

    Create Card.vue

---
+++ components/Card.vue
@@ -0,0 +1,13 @@
+<script setup>
+  
+</script>
+
+<template>
+ <div class="rounded-lg flex-col flex h-auto bg-gray-900/5 border-2 border-gray-900/5 min-h-120 p-4 animate-fade-in items-center justify-center backdrop-blur-md">
+    <slot/>
+ </div>
+</template>
+
+<style lang="scss" scoped>
+  
+</style>
