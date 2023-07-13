d14d0f8777ddbe137978bbffb6d56f9094dd94ab
Widdy LOUIS <louisws@outlook.fr>
2023-07-14 01:44:44 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,3 @@
-<script setup>
-  
-</script>
 <template>
   <NuxtLayout name="default">
     <div class="h-screen w-full flex items-center justify-center">
@@ -10,16 +7,12 @@
             <svg width="300" height="200" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 000 2h6a1 1 0 100-2H7z" clip-rule="evenodd"></path>
             </svg>
-            <nuxt-link class="button mt-8" to="">Login</nuxt-link>
+            <NuxtLink class="button mt-8" to="/auth/login">Connexion</NuxtLink>
             <span class="font-semibold mx-auto text-xs text-gray-900 inline-bold inline-block">or</span>
-            <nuxt-link class="button-secondary" to="">Create account</nuxt-link>
+            <NuxtLink class="button-secondary" to="/auth/register">Créer un compte</NuxtLink>
           </div>
         </Card>
       </div>
     </div>
   </NuxtLayout>
 </template>
-
-<style lang="scss" scoped>
-  
-</style>
