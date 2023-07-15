d97f6bf8433482fc91c33f051db15b874394b710
Widdy LOUIS <louisws@outlook.fr>
2023-07-15 22:20:34 +0200

    change the font image each time you change pages

--- components/BackgroundColor.vue
+++ components/BackgroundColor.vue
@@ -1,14 +1,13 @@
 <script setup>
   const route = useRoute();
   const img = ref('01.jpg');
+
+  watch(() => route.name, (curr,  prev) => {
+    const random = Math.floor(Math.random() * (6 - 1) + 1)
+    img.value = `/0${random}.jpg`
+  });
 </script>
 
 <template>
  <img loading="lazy" :src="img" class="h-full object-cover w-full absolute top-0 left-0 -z-1 animate-fade-in animate-duration-3000">
 </template>
-
-
-
-<style lang="scss" scoped>
-  
-</style>
