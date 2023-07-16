335c429e6fd774783a628ddbe6788dedcbf937c6
Widdy LOUIS <louisws@outlook.fr>
2023-07-16 14:34:24 +0200

    component import(AuthHeader)

---
+++ components/Auth/LoginForm.vue
@@ -0,0 +1,15 @@
+<script setup>
+  
+</script>
+
+<template>
+ <div class="w-full">
+  <AuthHeader title="bienvenue" subtitle="veuillez-vous connecter à votre compte"/>
+ </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+  
+</style>
