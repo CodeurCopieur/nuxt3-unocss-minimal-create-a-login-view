3e1c5a0cc5ed2e74baa8db2edf4d2a174659e4ff
Widdy LOUIS <louisws@outlook.fr>
2023-07-16 15:58:04 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,8 @@
 <template>
-  <!-- <div class="flex flex-col bg-gray-900 text-white"> -->
- 
-     <!-- <Header/> -->
-     <main class="w-full min-h-screen relative">
-        <div class="relative">
-          <slot />
-        </div>
-     </main>
-     <!-- <Footer/> -->
-  <!-- </div> -->
+
+   <main class="w-full min-h-screen relative">
+      <div class="relative">
+         <slot />
+      </div>
+   </main>
  </template>
