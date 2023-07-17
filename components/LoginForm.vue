dd62cda0f4bd9108c0189b0cc22735da6e713fba
Widdy LOUIS <louisws@outlook.fr>
2023-07-17 22:23:30 +0200

    Create LoginForm.vue

---
+++ components/LoginForm.vue
@@ -0,0 +1,12 @@
+<template>
+ <div class="w-full">
+  <CustomHeader title="bienvenue!!" subtitle="veuillez-vous connecter à votre compte" />
+  <form class="flex flex-col mt-8 w-full gap-4">
+    <div class="flex flex-col relative">
+      <label for="email" class="font-semibold font-sans text-sm text-gray-900">Email</label>
+      <input type="email" name="email" class="input">
+    </div>
+  </form>
+ </div>
+</template>
+
